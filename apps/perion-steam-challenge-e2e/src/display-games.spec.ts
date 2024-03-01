85b061d50842c4ccbda3054756fbb2348cfa7f0f
Michael Muscat <michael.muscat@outlook.com>
2024-03-01 13:14:06 +1100

    refactor: show games list from steam api using steam id or vanity name

---
+++ apps/perion-steam-challenge-e2e/src/display-games.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('should show my games', async ({ page }) => {
+  await page.goto('/');
+
+  const steamIdInput = page.getByLabel('Enter your steam ID')
+  const searchButton = page.getByTestId('search')
+  const gameList = page.getByTestId('game-list')
+  const gameCount = page.getByTestId('game-count')
+
+  await steamIdInput.pressSequentially('monkyyy')
+  await searchButton.click()
+
+  await expect(gameList).not.toHaveCount(0)
+  await expect(gameCount).not.toHaveCount(0)
+})
--- apps/perion-steam-challenge/next.config.js
+++ apps/perion-steam-challenge/next.config.js
@@ -12,6 +12,11 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+  logging: {
+    fetches: {
+      fullUrl: true
+    }
+  }
 };
 
 const plugins = [
